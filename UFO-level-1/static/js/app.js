66bd32a7bc146e1f753feacb8159414a37718d5f
Heba Ahmed <hebamohsen04@gmail.com>
2020-06-17 01:38:02 -0400

    Completed Part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,55 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+filteredData = tableData.filter(function(item){
+    return true;
+});
+
+
+d3.select("#filter-btn").on("click", function(){
+    var datetimeElement = d3.select("#datetime")
+    var inputValue = datetimeElement.property("value");
+
+    filteredData = tableData.filter(function(item){
+        if(inputValue == "") return true;
+        return item.datetime == inputValue;
+    });
+    createRows();
+});
+
+
+function createRows(){
+    //remove all old rows
+    d3.select("#table-body").selectAll("tr").remove();
+
+    //loop over all the data to create the new rows based on the filteredData
+    filteredData.forEach(function(item){
+        var tbody = d3.select("#table-body");
+        var row = tbody.append("tr"); //create row
+
+        var cell = row.append("td"); //create date column
+        cell.text(item.datetime);
+
+        var cell = row.append("td"); //create city column
+        cell.text(item.city);
+
+        var cell = row.append("td");
+        cell.text(item.state);
+
+        var cell = row.append("td");
+        cell.text(item.country);
+
+        var cell = row.append("td");
+        cell.text(item.shape);
+
+        var cell = row.append("td");
+        cell.text(item.durationMinutes);
+
+        var cell = row.append("td");
+        cell.text(item.comments);
+
+    });
+}
+
+createRows();
+
